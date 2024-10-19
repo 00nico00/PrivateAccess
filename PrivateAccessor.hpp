f4f29f9196bc7d0f0817e2b383dd4172dea10a0d
nico233 <1601550204@qq.com>
2024-10-19 18:31:15 +0800

    修正命名，添加私有成员方法的access，包含无参和有参的

--- PrivateAccessor.hpp
+++ PrivateAccessor.hpp
@@ -4,24 +4,86 @@
 namespace PrivateAccess
 {
     template <typename Class_t, typename Member_t, Member_t Class_t::* Member>
-    struct Accessor
+    struct PropertyAccessor
     {
-        using PLACEHOLDER_TYPE = Member_t*;
+        using Flag_t = Member_t*;
 
-        friend Member_t& access(Class_t& obj, PLACEHOLDER_TYPE)
+        friend Member_t& access(Class_t& obj, Flag_t)
         {
             return obj.*Member;
         }
     };
 
     template <typename T>
-    constexpr static T* PlaceHolder = static_cast<T*>(nullptr);
+    constexpr static T* Flag = nullptr;
 
-#define DECLARE_ACCESS(CLASS_TYPE, PROPERTY_TYPE, PROPERTY_NAME)                    \
-    namespace PrivateAccess                                                         \
-    {                                                                               \
-    template class Accessor<CLASS_TYPE, PROPERTY_TYPE, &CLASS_TYPE::PROPERTY_NAME>; \
-    PROPERTY_TYPE& access(CLASS_TYPE&, PROPERTY_TYPE*);                             \
+#ifdef DECLARE_PROPERTY_ACCESSOR
+#error DECLARE_PROPERTY_ACCESSOR macro redefined!
+#endif
+
+#define DECLARE_PROPERTY_ACCESSOR(CLASS_TYPE, PROPERTY_TYPE, PROPERTY_NAME)                 \
+    namespace PrivateAccess                                                                 \
+    {                                                                                       \
+    template class PropertyAccessor<CLASS_TYPE, PROPERTY_TYPE, &CLASS_TYPE::PROPERTY_NAME>; \
+    PROPERTY_TYPE& access(CLASS_TYPE&, PROPERTY_TYPE*);                                     \
+    }
+
+    template <typename Class_t, typename Ret_t, Ret_t(Class_t::*Member)()>
+    struct FunctionAccessorWith0Parameter
+    {
+        using Flag_t = Ret_t*;
+
+        friend Ret_t call(Class_t& obj, Flag_t)
+        {
+            if constexpr (std::is_same_v<Ret_t, void>)
+            {
+                (obj.*Member)();
+            }
+            else
+            {
+                return (obj.*Member)();
+            }
+        }
+    };
+
+#ifdef DECLARE_FUNCTION_ACCESSOR_With_0_ARGS
+#error DECLARE_FUNCTION_ACCESSOR_With_0_ARGS macro redefined!
+#endif
+
+#define DECLARE_FUNCTION_ACCESSOR_With_0_ARGS(CLASS_TYPE, RETURN_TYPE, FUNCTION_NAME)                   \
+    namespace PrivateAccess                                                                             \
+    {                                                                                                   \
+    template class FunctionAccessorWith0Parameter<CLASS_TYPE, RETURN_TYPE, &CLASS_TYPE::FUNCTION_NAME>; \
+    RETURN_TYPE call(CLASS_TYPE&, RETURN_TYPE*);                                                        \
+    }
+
+    template <typename Class_t, typename Ret_t, typename Parameter_t, Ret_t(Class_t::*Member)(Parameter_t)>
+    struct FunctionAccessorWith1Parameter
+    {
+        using Flag_t = Ret_t*;
+
+        friend Ret_t call(Class_t& obj, Parameter_t parameter, Flag_t)
+        {
+            if constexpr (std::is_same_v<Ret_t, void>)
+            {
+                (obj.*Member)(parameter);
+            }
+            else
+            {
+                return (obj.*Member)(parameter);
+            }
+        }
+    };
+
+#ifdef DECLARE_FUNCTION_ACCESSOR_With_1_ARGS
+#error DECLARE_FUNCTION_ACCESSOR_With_1_ARGS macro redefined!
+#endif
+
+#define DECLARE_FUNCTION_ACCESSOR_With_1_ARGS(CLASS_TYPE, RETURN_TYPE, PARAMETER_TYPE, FUNCTION_NAME)                   \
+    namespace PrivateAccess                                                                                             \
+    {                                                                                                                   \
+    template class FunctionAccessorWith1Parameter<CLASS_TYPE, RETURN_TYPE, PARAMETER_TYPE, &CLASS_TYPE::FUNCTION_NAME>; \
+    RETURN_TYPE call(CLASS_TYPE&, PARAMETER_TYPE, RETURN_TYPE*);                                                        \
     }
 
 }
--- main.cpp
+++ main.cpp
@@ -7,16 +7,29 @@ private:
     int a = 1;
     double b = 2.5;
     std::string str = "hello";
+
+    int func(int& para)
+    {
+        std::cout << a + para << "\n";
+        para = 2;
+        return a + para;
+    }
 };
 
-DECLARE_ACCESS(X, int, a)
-DECLARE_ACCESS(X, double, b)
-DECLARE_ACCESS(X, std::string, str)
+DECLARE_PROPERTY_ACCESSOR(X, int, a)
+DECLARE_PROPERTY_ACCESSOR(X, double, b)
+DECLARE_PROPERTY_ACCESSOR(X, std::string, str)
+
+DECLARE_FUNCTION_ACCESSOR_With_1_ARGS(X, int, int&, func)
 
 int main()
 {
     X x;
-    std::cout << PrivateAccess::access(x, PrivateAccess::PlaceHolder<int>) << "\n";
-    std::cout << PrivateAccess::access(x, PrivateAccess::PlaceHolder<double>) << "\n";
-    std::cout << PrivateAccess::access(x, PrivateAccess::PlaceHolder<std::string>) << "\n";
+    std::cout << PrivateAccess::access(x, PrivateAccess::Flag<int>) << "\n";
+    std::cout << PrivateAccess::access(x, PrivateAccess::Flag<double>) << "\n";
+    std::cout << PrivateAccess::access(x, PrivateAccess::Flag<std::string>) << "\n";
+
+    int b = 1;
+    int res = PrivateAccess::call(x, b, PrivateAccess::Flag<int>);
+    std::cout << "res: " << res << ", b: " << b << "\n";
 }
